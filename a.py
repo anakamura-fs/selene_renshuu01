d83b89ad3ac5c4f509f160d5fda254a6e68dd92e
naka <naka@example.com>
2021-07-25 21:14:12 +0900

    first selene script.
    copied from https://github.com/yashaka/selene "quick start"

--- a.py
+++ a.py
@@ -1,6 +1,9 @@
+from selene.support.shared import browser
+from selene import by, be, have
 
-a=1
-a+=1
-a+=2
-a+=3
-print(a)
+browser.open('https://google.com/ncr')
+browser.element(by.name('q')).should(be.blank)\
+    .type('selenium').press_enter()
+browser.all('.srg .g').should(have.size(10))\
+    .first.should(have.text('Selenium automates browsers'))
+
