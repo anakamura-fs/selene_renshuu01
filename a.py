fe8d8f4fdbb25e5665950928bc35bade44275c79
naka <naka@example.com>
2021-09-12 18:53:26 +0900

    現状にあわせてgoogleに与えるパラメータを少し変えた。GREEN.

--- a.py
+++ a.py
@@ -4,6 +4,6 @@ from selene import by, be, have
 browser.open('https://google.com/ncr')
 browser.element(by.name('q')).should(be.blank)\
     .type('selenium').press_enter()
-browser.all('.srg .g').should(have.size(10))\
+browser.all('.g').should(have.size(16))\
     .first.should(have.text('Selenium automates browsers'))
 
