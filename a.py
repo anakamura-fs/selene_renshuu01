069c1a8f60ad3ce052664b7d2375efbeaded3603
naka <naka@example.com>
2021-09-14 19:45:31 +0900

    insecure (おれおれ証明書)対応。todo: driverを手動で用意したくない。managementを使いたい。

--- a.py
+++ a.py
@@ -1,6 +1,15 @@
 from selene.support.shared import browser
 from selene import by, be, have
 
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+capabilities = DesiredCapabilities.CHROME.copy()
+capabilities['acceptInsecureCerts'] = True
+
+driver = Chrome(desired_capabilities=capabilities, executable_path=r'C:\chromedriver.exe')
+browser.set_driver(driver)
+
 browser.open('https://google.com/ncr')
 browser.element(by.name('q')).should(be.blank)\
     .type('selenium').press_enter()
